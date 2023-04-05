620930f0a984d09482030161908476625d5a765f
Dev Prasad Bashyal <devpb1998@gmail.com>
2023-04-05 20:21:54 +0530

    Spring core Project

---
+++ src/main/java/com/springcore/collections/Test.java
@@ -0,0 +1,14 @@
+package com.springcore.collections;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/collections/collectionsconfig.xml");
+		Emp emp1 = (Emp) context.getBean("emp1");
+		System.out.println(emp1);
+	}
+
+}
---
+++ src/main/java/com/springcore/springcore/App.java
@@ -0,0 +1,19 @@
+package com.springcore.springcore;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student student = (Student) context.getBean("student1");
+        student.setStudentId(234);
+        System.out.println(student);
+    }
+}
